fdd772a76c6582ceb65d8cf95897467b1168f1a6
lzq <renovil@163.com>
2017-06-24 11:46:58 +0800

    send message

--- src/main/Java/io/rong/SendText.java
+++ src/main/Java/io/rong/SendText.java
@@ -11,14 +11,17 @@ import io.rong.models.CodeSuccessResult;
  */
 public class SendText {
     public static void main(String[] args) throws Exception {
-        String appKey = "8luwapkv8t3pl";//替换成您的appkey
-        String appSecret = "z0gFndOLga2Q";//替换成匹配上面key的secret
+        String appKey = "8luwapkv8t3pl";
+        String appSecret = "z0gFndOLga2Q";
         RongCloud rongCloud = RongCloud.getInstance(appKey, appSecret);
 
         String[] messagePublishPrivateToUserId = {"webuser"};
-        String toWeb = "hello, webuser, test";
+        String toWeb = "hello, for test";
         TxtMessage messagePublishPrivateVoiceMessage = new TxtMessage(toWeb, "helloExtra");
-        CodeSuccessResult messagePublishPrivateResult = rongCloud.message.publishPrivate("serverUser", messagePublishPrivateToUserId, messagePublishPrivateVoiceMessage, "thisisapush", "{\"pushData\":\"hello\"}", "4", 0, 0, 0, 0);
+        CodeSuccessResult messagePublishPrivateResult = rongCloud.message.publishPrivate("4700299477",
+                messagePublishPrivateToUserId, messagePublishPrivateVoiceMessage, "thisisapush",
+                "{\"pushData\":\"hello\"}", "4", 0, 0, 0, 0);
         System.out.println("publishPrivate:  " + messagePublishPrivateResult.toString());
+
     }
 }
