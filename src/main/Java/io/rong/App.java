1c6c7ba1a8495475d20ab7bcd54883ff25d67cbd
lzq <renovil@163.com>
2017-06-27 20:28:36 +0800

    parse message

---
+++ src/main/Java/io/rong/App.java
@@ -0,0 +1,15 @@
+package io.rong;
+
+/**
+ * @Author:lzq
+ * @Discription
+ * @Date: Created on 2017/6/1.
+ * @Modified By:
+ */
+class App {
+    static final String SERVER_USERID = "watchserver";
+
+    static final String APP_KEY = "8luwapkv8t3pl";
+    static final String APP_SECRET = "z0gFndOLga2Q";
+
+}
